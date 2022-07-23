492e0db493e1f1d525a816430ecf2ff4d75c8cb7
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2022-07-23 10:39:53 -0300

    Condicional boolean indexado

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
-
-export default function Home() {
+export default function Home({data}) {
+console.log(data.alive);
   return (
 		<div>
-			<h1>Doação de computadores usados</h1>
+			<h1>
+				Doação de computadores usados
+			</h1>
+			<p>
+				{data.alive === true ? '{alive:true}' : '{alive:false}'}
+	  		</p>
 		</div>
   )
 }
+
+export async function getStaticProps() {
+	const response = await fetch('https://doar-computador-api.herokuapp.com/')
+	const data = await response.json();
+
+	return {
+		props: {data},
+		
+	}
+}
+  
